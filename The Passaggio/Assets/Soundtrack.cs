dc3b4f3c34e102da6f2f04d003f9550ae3d93697
Wiktor Wasowski <wiktor.aaron.wasowski@gmail.com>
2019-08-26 07:38:39 +0200

    Add music

---
+++ The Passaggio/Assets/Soundtrack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundtrack : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+
+
+    private void Awake()
+    {
+    }
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void Start()
+    {
+        Play();
+    }
+
+    public void Play()
+    {
+        audioSource.Play();
+    }
+}
