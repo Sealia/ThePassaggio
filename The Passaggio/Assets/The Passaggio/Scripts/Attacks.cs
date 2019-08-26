1f9c97b7e2285bcbbf39ca277baf26c01dfd3aef
Sealia <sealiaborusiak@wp.pl>
2019-08-26 14:55:02 +0200

    Update Tentacles

--- The Passaggio/Assets/The Passaggio/Scripts/Attacks.cs
+++ The Passaggio/Assets/The Passaggio/Scripts/Attacks.cs
@@ -20,6 +20,7 @@ public class Attacks : MonoBehaviour
     public float spawny = 2;
 
     Vector3 spawnPos;
+    int num;
 
 
 
@@ -51,88 +52,43 @@ public class Attacks : MonoBehaviour
     {
         while(true)
         {
-             int num = Random.Range(0, 1);
-            num = 0;
-             
-             if (num == 0)
-             {
-                spawnPos = new Vector3(Random.Range(bridge.transform.position.x - 2.5f, bridge.transform.position.x - 4.5f), -30, Random.Range(spawnRangeZ+15, spawnRangeZ + 25));
-                if ((spawnPos.z > bridge.transform.position.z - 0.5f || spawnPos.z < bridge.transform.position.z + 0.5f) && spawnPos.x > 3.5f)
-                 {
-                     spawnPos.x -= 1;
-                 }
-
-                 Instantiate(Prefabs[0], spawnPos, Prefabs[0].transform.rotation);
-             }
-             else if (num == 1)
-             {
-                spawnPos = new Vector3(Random.Range(bridge.transform.position.x + 2.5f, bridge.transform.position.x + 4.5f), -30, Random.Range(spawnRangeZ, spawnRangeZ + 1));
-
-                if ((spawnPos.z > bridge.transform.position.z - 0.5f || spawnPos.z < bridge.transform.position.z + 0.5f) && spawnPos.x < 3.5f)
-                 {
-                     spawnPos.x += 1;
-                 }
-                 Instantiate(Prefabs[0], spawnPos, Quaternion.Euler(0f, 180, 0f) );
-
-             }
-
-
-            yield return new WaitForSeconds(2);
-        }
-        
-    }
-void RandomAttack()
-{
-/* if(stats.isDead==false)
- {
-     int Index = Random.Range(0, Prefabs.Length);
-
-   //  Vector3 spawnPos = new Vector3(spawnPosX, 0.5f, Random.Range(spawnRangeZ, spawnRangeZ + 4));
-    /* if (Prefabs[Index].name == "Sphere")
-     {
-         int num = Random.Range(1, 3);
-         Instantiate(Prefabs[Index], spawnPos, Prefabs[Index].transform.rotation);
-         for (int i = 0; i < num; i++)
-         {
-             Instantiate(Prefabs[Index], spawnPos + new Vector3(0, 0, 2), Prefabs[Index].transform.rotation);
-         }
-
-     }*/
-        //  if (Prefabs[Index].name == "Tentacle")
-        // {
-
-        //   int num = Random.Range(0, 1);
-        //   if (num == 0)
-        //   {
-        /*    Vector3 spawnPos = new Vector3(Random.Range(bridge.transform.position.x + 2.5f, bridge.transform.position.x+4.5f), -30, Random.Range(spawnRangeZ, spawnRangeZ + 1));
-
-            if((spawnPos.z>bridge.transform.position.z-0.5f || spawnPos.z<bridge.transform.position.z+0.5f) && spawnPos.x<3.5f)
+            int tentaclesNumber = Random.RandomRange(1, 4);
+            
+            for(int i=0; i<tentaclesNumber;i++)
             {
-                spawnPos.x += 1;
-
-            }*/
-        Vector3 spawnPos = new Vector3(spawnX, -30, spawny);
-
-        Instantiate(Prefabs[0], spawnPos, Prefabs[0].transform.rotation);
-        spawny += 2;
-              //  }
-               /* if(num == 1)
+                num = Random.Range(0, 2);
+                float direction = Random.Range(-45, 45);
+                if (num == 0)
                 {
-                    Vector3 spawnPos = new Vector3(Random.Range(bridge.transform.position.x-4.5f, bridge.transform.position.x-2.5f), -30, Random.Range(spawnRangeZ, spawnRangeZ + 4));
+                    spawnPos = new Vector3(Random.Range(bridge.transform.position.x - 2.5f, bridge.transform.position.x - 4.5f), -30, Random.Range(spawnRangeZ + 15, spawnRangeZ + 25));
+                    if ((spawnPos.z > bridge.transform.position.z - 0.5f || spawnPos.z < bridge.transform.position.z + 0.5f) && spawnPos.x > 3.5f)
+                    {
+                        spawnPos.x -= 1;
+                    }
+
+                    Instantiate(Prefabs[0], spawnPos, Quaternion.Euler(0f, direction, 0f));
+                }
+                else if (num == 1)
+                {
+                    spawnPos = new Vector3(Random.Range(bridge.transform.position.x + 2.5f, bridge.transform.position.x + 4.5f), -30, Random.Range(spawnRangeZ, spawnRangeZ + 1));
 
                     if ((spawnPos.z > bridge.transform.position.z - 0.5f || spawnPos.z < bridge.transform.position.z + 0.5f) && spawnPos.x < 3.5f)
                     {
-                        spawnPos.x -= 1;
-
+                        spawnPos.x += 1;
                     }
-                    Instantiate(Prefabs[Index], spawnPos, Prefabs[Index].transform.rotation);
-                }*/
-          //  }
-     //   }
+                    Instantiate(Prefabs[0], spawnPos, Quaternion.Euler(0f, direction + 180f, 0f));
+
+                }
+
+                yield return new WaitForSeconds(0.5f);
+            }
+             
+
+
+            yield return new WaitForSeconds(8);
+        }
         
-
-
-
-
     }
+
+    
 }
