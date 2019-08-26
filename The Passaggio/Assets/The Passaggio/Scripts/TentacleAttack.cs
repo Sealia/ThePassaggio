6e516a43ff3bfa32940056c4a4648608ca3ad8b7
Sealia <sealiaborusiak@wp.pl>
2019-08-27 00:07:25 +0200

    Fix Tentacle Destroying

--- The Passaggio/Assets/The Passaggio/Scripts/TentacleAttack.cs
+++ The Passaggio/Assets/The Passaggio/Scripts/TentacleAttack.cs
@@ -7,6 +7,9 @@ public class TentacleAttack : MonoBehaviour
     Animator anim;
     GameObject player;
     PlayerStats death;
+    List<int> column = new List<int>() { 24, 25, 30, 31, 32, 33, 38, 39 };
+    Bridge bridge;
+    GameObject br;
 
     public AudioClip[] impactSounds;
 
@@ -18,6 +21,8 @@ public class TentacleAttack : MonoBehaviour
         anim = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player");
         death = GetComponent<PlayerStats>();
+        br = GameObject.Find("Bridge");
+        bridge = br.GetComponent<Bridge>();
     }
     // Start is called before the first frame update
     void Start()
@@ -38,7 +43,7 @@ public class TentacleAttack : MonoBehaviour
     {
         if(other.transform.parent.tag=="Tile")
         {
-           
+
             other.transform.parent.GetChild(0).GetComponent<Rigidbody>().isKinematic = false;
             other.gameObject.transform.gameObject.SetActive(false);
 
@@ -53,7 +58,7 @@ public class TentacleAttack : MonoBehaviour
 
     IEnumerator Erease()
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(5);
         Destroy(gameObject);
     }
 
